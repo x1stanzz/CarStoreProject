bf5e923d7ec784c90771de9ff2fef32d7b67e1c6
x1stanzz <vova28032003@gmail.com>
2024-05-12 17:34:15 +0300

    Bottom navigation bar

--- app/src/main/java/com/example/carstoreproject/screens/MainScreen.kt
+++ app/src/main/java/com/example/carstoreproject/screens/MainScreen.kt
@@ -1,19 +1,84 @@
 package com.example.carstoreproject.screens
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 
+data class BottomNavigationItem(
+    val title: String,
+    val icon: ImageVector
+)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier
 ) {
-    Surface(
-        color = Color.White,
-        modifier = Modifier.fillMaxSize()
+    val items = listOf(
+        BottomNavigationItem(
+            title = "Home",
+            icon = Icons.Filled.Home
+        ),
+        BottomNavigationItem(
+            title = "Search",
+            icon = Icons.Filled.Search
+        ),
+        BottomNavigationItem(
+            title = "Favorite",
+            icon = Icons.Filled.Star
+        ),
+        BottomNavigationItem(
+            title = "Settings",
+            icon = Icons.Filled.Settings
+        )
+    )
+    var selectedItem by rememberSaveable { mutableStateOf(0)}
+    Scaffold(
+        bottomBar = {
+            NavigationBar(
+                containerColor = Color.White
+            ) {
+                items.forEachIndexed() { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItem == index,
+                        onClick = {
+                            selectedItem = index
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.title
+                            )
+                        },
+                        label = {
+                            Text(text = item.title)
+                        },
+                        colors = NavigationBarItemDefaults.colors(
+                            selectedIconColor = Color(0xff6789ba),
+                            selectedTextColor = Color(0xff6789ba),
+                            indicatorColor = Color.White
+                        )
+                    )
+                }
+            }
+        }
     ) {
 
     }
